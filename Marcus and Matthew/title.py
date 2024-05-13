b5fec37a7fb4ff3ee8d8d58c62fb212cf95d06f1
matthewt4-6-6 <turnermatthew954@gmail.com>
2024-05-13 13:30:25 -0700

    Update title.py

--- Marcus and Matthew/title.py
+++ Marcus and Matthew/title.py
@@ -4,7 +4,12 @@ def title():
     print("                                                              | (__ / _ \| |_| (_|___/ _ \|  _|  _/")
     print("                                                               \___/_/ \_|____\___| /_/ \_|_| |_|  ")
     print("                                                                       BY MARCUS AND MATTHEW")
-   
+    print("")
+    print("")
+    print("                                                                              OPTIONS")
+    print("")
+    print("                                                         1 -  Area Calculations     2 - Surface Area Calculations")
+
 
 title()
 
