25b3ac5ea6405c2fdc134df11b52299d198267da
matthewt4-6-6 <turnermatthew954@gmail.com>
2024-05-13 13:18:48 -0700

    Update title.py

--- Marcus and Matthew/title.py
+++ Marcus and Matthew/title.py
@@ -1,1 +1,12 @@
-def title()
+def title():
+    print("                                                                ___   _   _    ___     _   ___ ___ ")
+    print("                                                               / __| /_\ | |  / _____ /_\ | _ | _ \ ")
+    print("                                                              | (__ / _ \| |_| (_|___/ _ \|  _|  _/")
+    print("                                                               \___/_/ \_|____\___| /_/ \_|_| |_|  ")
+    print("                                                                       BY MARCUS AND MATTHEW")
+   
+
+title()
+
+
+                                        
