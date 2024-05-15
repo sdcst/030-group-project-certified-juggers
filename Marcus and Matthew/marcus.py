0e1f47a4fe291e496d50eb7ff8185f3b9a4ce93c
marcuswhenit <159051524+marcuswhenit@users.noreply.github.com>
2024-05-15 13:29:21 -0700

    almost finished

--- Marcus and Matthew/marcus.py
+++ Marcus and Matthew/marcus.py
@@ -2,7 +2,24 @@
 #volume calculations
 
 
+def volRectPris():
+    l = input("enter a length > ")
+    l=float(l)
+    w = input("enter a width > ")
+    w=float(w)
+    h = input("enter a height > ")
+    h=float(h)
+    
+    
 
-input("enter a length")
-input("enter a width")
-input("enter a height")
+    if l  < 0 or w < 0 or h < 0:
+        print("you cant do that")
+
+    if l > 0 and w > 0 and h > 0:
+        print(f"your volume is now {l*w*h}")
+        return
+    else:
+        print("not number")
+        return
+
+volRectPris()
