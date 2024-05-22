98e58f4b72815bf0cc247727c1c45cee3fba97f7
marcuswhenit <159051524+marcuswhenit@users.noreply.github.com>
2024-05-22 13:31:04 -0700

    all

--- taxcalc.py
+++ taxcalc.py
@@ -1,1 +1,12 @@
 
+import math
+
+def taxCalc():
+    pst = 0.07
+    gst = 0.05
+    income = float(input("enter subtotal > "))
+    print(f"your PST total is {income+income * pst}")
+    print(f"your PST total is {income+income * gst}")
+    print(f"your FULL total is {income+income * (pst+gst)}")
+
+taxCalc()
