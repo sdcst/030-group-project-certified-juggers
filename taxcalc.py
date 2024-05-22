818ed43e42f2631ce7565af0d083819f5df5e7d0
marcuswhenit <159051524+marcuswhenit@users.noreply.github.com>
2024-05-22 13:35:35 -0700

    all

--- taxcalc.py
+++ taxcalc.py
@@ -2,11 +2,19 @@
 import math
 
 def taxCalc():
-    pst = 0.07
-    gst = 0.05
-    income = float(input("enter subtotal > "))
-    print(f"your PST total is {income+income * pst}")
-    print(f"your PST total is {income+income * gst}")
-    print(f"your FULL total is {income+income * (pst+gst)}")
+    try:
+        pst = 0.07
+        gst = 0.05
+        income = float(input("enter subtotal > "))
+        print(f"your PST total is {income+income * pst}")
+        print(f"your PST total is {income+income * gst}")
+        print(f"your FULL total is {income+income * (pst+gst)}")
+        input("                                                                              press 'enter' to exit")
+        return
+
+    except:
+        print("                                                                                 not number")
+        input()
+        return    
 
 taxCalc()
